56d6829cfb6f70143651107a6a0d8fe7c0d501f6
KonggMinhh <hoangkongminh2001@gmail.com>
2023-12-23 01:09:16 +0700

    fix vercel

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import * as process from 'process';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(process.env.PORT, '0.0.0.0');
+    await app.listen(process.env.PORT || 3000);
     if (module.hot) {
         module.hot.accept();
         module.hot.dispose(() => app.close());
